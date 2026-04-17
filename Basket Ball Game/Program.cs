a294574ef213784dd30ab0477a92beb715f2d33d
God-04 <Tobias@bitprogress.com>
2026-04-17 10:09:21 +0200

    Added Hello on start

--- Basket Ball Game/Program.cs
+++ Basket Ball Game/Program.cs
@@ -8,6 +8,7 @@ namespace Basket_Ball_Game
         [STAThread]
         static void Main()
         {
+            Console.WriteLine("Hello");
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
