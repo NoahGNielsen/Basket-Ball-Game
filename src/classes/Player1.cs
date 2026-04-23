5f72b1c49843a042eb1e83907d5b190f6c94b676
God-04 <Tobias@bitprogress.com>
2026-04-23 13:50:19 +0200

    Added varriables on P1

--- src/classes/Player1.cs
+++ src/classes/Player1.cs
@@ -8,6 +8,14 @@ namespace Basket_Ball_Game
     {
         int? speed = GlobalConfig.playerMovementSpeed;
         int? JHigh = GlobalConfig.playerMovementJumpHeight;
+        int gx = GlobalConfig.gameSizeX;
+        int gy = GlobalConfig.gameSizeY;
+
+
+        int pX;
+        int pY;
+
+        Player1 p1 = new Player1();
 
 
     }
