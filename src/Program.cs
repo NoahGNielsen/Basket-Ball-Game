3c6adc342f924fe960c4a5ffacf9867a98cb2784
God-04 <Tobias@bitprogress.com>
2026-05-26 21:52:22 +0200

    Wtf did you do????

--- src/Program.cs
+++ src/Program.cs
@@ -82,7 +82,7 @@ namespace Basket_Ball_Game
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form1()); //
         }
     }
 }
